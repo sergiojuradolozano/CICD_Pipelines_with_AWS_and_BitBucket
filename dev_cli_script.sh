fdde0c1fd4a70420eb868b2e7f2bbcb1188cdcf4
sergiolozano <sergio.lozano@ultumus.com>
2023-10-20 16:28:52 +0100

    Changes: - Update Scripts - Add new scripts for uat and prod

--- dev_cli_script.sh
+++ dev_cli_script.sh
@@ -1,8 +1,11 @@
 #RUN THIS SCRIPT IN THE LOCATION WHERE THE FILES ARE IN.
 
-#CREATE OR UPDATE THE CLOUDFORMATION STACK OF THE AURORA CODEPIPELINE ROLE
-aws cloudformation deploy --template-file ./Aurora-Postgres-Serverless-V2-CodePipeline-Role.yaml --stack-name Aurora-Postgres-Serverless-V2-CodePipeline-Role --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM --parameter-overrides Environment=dev Region=us-east-1 #See comment on the Region parameter in the cloudformation stacks.
+#CREATE OR UPDATE THE CLOUDFORMATION STACK OF THE Pipelines Role
+aws cloudformation deploy --template-file ./Pipelines-Role/CodePipelines-Role.yaml --stack-name CodePipelines-Role --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM --parameter-overrides Environment=dev Region=us-east-1 #See comment on the Region parameter in the cloudformation stacks.
+
+#CREATE OR UPDATE THE CLOUDFORMATION STACK OF THE CORE NETWORK
+aws cloudformation deploy --template-file ./Pipelines/Core-Network-CodePipeline.yaml --stack-name Core-Network-CodePipeline --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM --parameter-overrides Environment=dev Region=us-east-1 #See comment on the Region parameter in the cloudformation stacks.
 
 #CREATE OR UPDATE THE CLOUDFORMATION STACK OF THE AURORA CODEPIPELINE
-aws cloudformation deploy --template-file ./Aurora-Postgres-Serverless-V2-CodePipeline.yaml --stack-name Aurora-Postgres-Serverless-V2-CodePipeline --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM --parameter-overrides Environment=dev Region=us-east-1 #See comment on the Region parameter in the cloudformation stacks.
+aws cloudformation deploy --template-file ./Pipelines/Aurora-Postgres-Serverless-V2-CodePipeline.yaml --stack-name Aurora-Postgres-Serverless-V2-CodePipeline --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM --parameter-overrides Environment=dev Region=us-east-1 #See comment on the Region parameter in the cloudformation stacks.
 
