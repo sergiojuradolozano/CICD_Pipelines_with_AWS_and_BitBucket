c4410f6fa501c64b159847c6fd6185df60a9dec0
sergiolozano <sergio.lozano@ultumus.com>
2023-10-21 12:00:54 +0100

    Changes: - Add S3 bucket cli deployment

--- dev_cli_script.sh
+++ dev_cli_script.sh
@@ -1,5 +1,8 @@
 #RUN THIS SCRIPT IN THE LOCATION WHERE THE FILES ARE IN.
 
+#CREATE the S3 Bucket for the pipelines artifact
+aws cloudformation deploy --template-file ./Pipelines/CodePipelineS3ArtifactsBucket.yaml --stack-name CodePipelineS3ArtifactsBucket --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM --parameter-overrides Environment=dev Region=us-east-1 #See comment on the Region parameter in the cloudformation stacks.
+
 #CREATE OR UPDATE THE CLOUDFORMATION STACK OF THE Pipelines Role
 aws cloudformation deploy --template-file ./Pipelines-Role/CodePipelines-Role.yaml --stack-name CodePipelines-Role --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM --parameter-overrides Environment=dev Region=us-east-1 #See comment on the Region parameter in the cloudformation stacks.
 
