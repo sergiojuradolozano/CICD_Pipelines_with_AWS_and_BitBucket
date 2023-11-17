99ea482a76c0393673474b2774af6ba264e632a4
sergiolozano <sergio.lozano@ultumus.com>
2023-11-17 06:34:31 +0000

    add files

--- dev_cli_script.sh
+++ dev_cli_script.sh
@@ -6,9 +6,9 @@ aws cloudformation deploy --template-file ./Pipelines/CodePipelineS3ArtifactsBuc
 #CREATE OR UPDATE THE CLOUDFORMATION STACK OF THE Pipelines Role
 aws cloudformation deploy --template-file ./Pipelines-Role/CodePipelines-Role.yaml --stack-name CodePipelines-Role --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM --parameter-overrides Environment=dev Region=us-east-1 #See comment on the Region parameter in the cloudformation stacks.
 
-#CREATE OR UPDATE THE CLOUDFORMATION STACK OF THE CORE NETWORK
+#CREATE OR UPDATE THE CLOUDFORMATION STACK OF THE CORE NETWORK PIPELINE
 aws cloudformation deploy --template-file ./Pipelines/Core-Network-CodePipeline.yaml --stack-name Core-Network-CodePipeline --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM --parameter-overrides Environment=dev Region=us-east-1 #See comment on the Region parameter in the cloudformation stacks.
 
-#CREATE OR UPDATE THE CLOUDFORMATION STACK OF THE AURORA CODEPIPELINE
-aws cloudformation deploy --template-file ./Pipelines/Aurora-Postgres-Serverless-V2-CodePipeline.yaml --stack-name Aurora-Postgres-Serverless-V2-CodePipeline --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM --parameter-overrides Environment=dev Region=us-east-1 #See comment on the Region parameter in the cloudformation stacks.
+#CREATE OR UPDATE THE CLOUDFORMATION STACK OF THE APPLICATION PIPELINE
+aws cloudformation deploy --template-file ./Pipelines/Application-CodePipeline.yaml --stack-name Application-CodePipeline --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM --parameter-overrides Environment=dev Region=us-east-1 #See comment on the Region parameter in the cloudformation stacks.
 
